149787897e72c443d48947b263c9aafe11f7f6e0
Ian J <ianbennettjohnson@gmail.com>
2020-06-04 15:01:57 -0700

    Created seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name) values('Big Greased up McCheese');
+INSERT INTO burgers(burger_name) values('Sickening Greased Chicken McBurger');
+INSERT INTO burgers(burger_name) values('Filthed Up Greased McGrease Burger');
