cbbcd27ac27aff9679c046b43a05f5c61be075ee
Ian J <ianbennettjohnson@gmail.com>
2020-06-04 15:52:43 -0700

    Updated connection.js

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+let connection;
+
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'burgers_db'
+    });
+};
+
+//Connection
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting ' + err.stack);
+        return;
+    }
+    console.log('connected as This ' + connection.threadId);
+});
+
+module.exports = connection;
