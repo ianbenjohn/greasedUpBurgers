3a89413dd50688b8400e4224cd80d7876c871113
Ian J <ianbennettjohnson@gmail.com>
2020-06-04 16:47:07 -0700

    Updating burger.js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,28 @@
+$(function() {
+    $(".devour_btn").on("click", function(event) {
+        const id = $(this).data("burgerid");
+
+        $.ajax("/api/burger/" + id, {
+            type: "PUT"
+        }).then(function() {
+            location.reload();
+        })
+    })
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        const newBurger = {
+            name: $("#burger").val().trim(),
+            devoured: false
+        };
+
+        $.ajax("/api/burger", {
+            type: "POST",
+            data: newBurger
+        }).then(function() {
+            console.log("created new burger");
+            location.reload();
+        });
+    });
+});
