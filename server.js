8dd4580280204616301e4367a493bed118477ad0
Ian J <ianbennettjohnson@gmail.com>
2020-06-04 17:14:12 -0700

    updating server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
